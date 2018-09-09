c3d0a5cdeb709bb209017928919d8fabb7b37e5b
Nicole B <contact@nebracy.com>
2018-09-09 13:07:29 -0400

    Add listener for screen size

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ document.getElementById("dropbtn").onclick = toggleDropdown;
 document.getElementById("dropdown").onclick = toggleDropdown;
 
 function toggleDropdown() {
-    var x = window.matchMedia("(max-width: 650px)");
+    var x = window.matchMedia("(max-width: 650px)");    // Less than 650px
     if (x.matches) {
         document.getElementById("dropdown").classList.toggle("nav__menu--show");
     }
 }
+
+function screenSize(width) {
+    if (width.matches) {     // Greater than 650px
+        var element = document.getElementById("dropdown");
+        if (element.classList.contains("nav__menu--show")) {
+            element.classList.remove("nav__menu--show");
+        }
+    }
+}
+
+var width = window.matchMedia("(min-width: 650px)");
+width.addListener(screenSize);
