5a8c3b43bdf3904f99a9105a02ab096a1d7e3996
Nicole B <contact@nebracy.com>
2018-10-04 16:07:23 -0400

    Add variable for page titles

--- mysite.py
+++ mysite.py
@@ -5,12 +5,12 @@ app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Home")
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('404.html', title="Page Not Found"), 404
 
 
 if __name__ == "__main__":
