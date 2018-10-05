9bf6e21f4bb07547d50ed7297d0f00a47c052ca5
Nicole B <contact@nebracy.com>
2018-10-05 15:01:07 -0400

    Replace honey field with flask honey field

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, Optional
 
 
 class ContactForm(FlaskForm):
@@ -8,4 +8,5 @@ class ContactForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     subj = StringField('Subject', validators=[DataRequired(), Length(max=70)])
     msg = TextAreaField('Message', validators=[DataRequired()])
+    honey = StringField('Website', validators=[Optional()])
     submit = SubmitField('Send')
