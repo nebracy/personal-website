04a449d142e8523b9c0e0decbaa2e827de9f3d14
Nicole B <contact@nebracy.com>
2018-10-05 15:22:44 -0400

    Change form action and add flask mail

--- mysite.py
+++ mysite.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from forms import ContactForm
+from flask_mail import Mail, Message
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py', silent=True)
 
+mail = Mail(app)
 
-@app.route("/")
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
     form = ContactForm()
+    if form.validate_on_submit():
+        msg = Message(form.subj.data, sender=(form.name.data, form.email.data), recipients=['contact@nebracy.com'],
+                      reply_to=form.email.data)
+        msg.body = form.msg.data
+        mail.send(msg)
+        return redirect(url_for('index'))
     return render_template('index.html', form=form, title="Home")
 
 
