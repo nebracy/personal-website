fc19239d92e45608461de75fd433c26d4f7eeee4
Nicole B <contact@nebracy.com>
2020-02-15 14:18:03 -0500

    Set default subdomain as 'test' if environment is not production

--- nebracy/__init__.py
+++ nebracy/__init__.py
@@ -20,6 +20,8 @@ def before_route(endpoint, values):
 env = os.getenv('FLASK_ENV', 'Production')
 app.config.from_object(f'config.{env}')
 app.config.from_pyfile('config.py', silent=True)
+if env != 'Production':
+    app.url_map.default_subdomain = 'test'
 db = SQLAlchemy(app)
 mail = Mail(app)
 
