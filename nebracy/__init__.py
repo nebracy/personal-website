0979c27d9e70156a2e85f933efac50c9eb770ae0
Nicole B <contact@nebracy.com>
2020-12-18 11:09:51 -0500

    Delete unused commented code

--- nebracy/__init__.py
+++ nebracy/__init__.py
@@ -7,26 +7,10 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 mail = Mail()
 
-# config_env = os.getenv('FLASK_ENV', 'Production')
-# if config_env == 'Production':
-#     static_subdomain = 'static'
-# elif config_env == 'Staging' or 'Development':
-#     static_subdomain = 'static2'
-
 
 def create_app():
     app = Flask(__name__, static_url_path='')
-    # app.add_url_rule('/<path:filename>',
-    #                  endpoint='static',
-    #                  subdomain='<static>',
-    #                  view_func=app.send_static_file)
-
     app.config.from_object(f'config.{os.getenv("FLASK_ENV", "Production")}')
-
-    # if env == 'Staging':
-    #     app.url_map.default_subdomain = 'test'
-    # elif env == 'Development':
-    #     app.url_map.default_subdomain = 'local'
 
     db.init_app(app)
     mail.init_app(app)
@@ -38,10 +22,3 @@ def create_app():
 
         db.create_all()
         return app
-
-
-# @app.url_value_preprocessor
-# def before_route(endpoint, values):
-#     if values is not None:
-#         values.pop('static', None)
-
