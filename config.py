96b37601a35b44ad6f41344b84e4d40733fb5219
Nicole B <contact@nebracy.com>
2020-12-25 15:31:27 -0500

    Update and reorder config variables

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 class Config:
     TESTING = False
-    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'default-secret-key')
-    MAIL_SERVER = os.getenv('FLASK_MAIL_SERVER', 'smtp.gmail.com')
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
     MAIL_PORT = 587
     MAIL_USE_TLS = True
+    MAIL_SERVER = os.getenv('FLASK_MAIL_SERVER', 'smtppro.zoho.com')
     MAIL_USERNAME = os.getenv('FLASK_MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('FLASK_MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.getenv('FLASK_MAIL_DEFAULT_SENDER')
@@ -17,16 +17,15 @@ class Config:
 
 class Development(Config):
     TESTING = True
-    # SERVER_NAME = 'local.nebracy.com:443'     # local flask run only
+    SECRET_KEY = 'default-secret-key'
+    MAIL_SERVER = 'smtp.gmail.com'
+    # SERVER_NAME = 'local.nebracy.com:443'
 
 
 class Staging(Config):
     TESTING = True
     S3_FOLDER = 'static/staging'
-    # SERVER_NAME = 'test.nebracy.com:443'
 
 
 class Production(Config):
-    SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
-    MAIL_SERVER = os.getenv('FLASK_MAIL_SERVER', 'smtppro.zoho.com')
     S3_FOLDER = 'static/production'
