55c0c652f63359104115913b21ec9ef63956c3f2
Nicole B <contact@nebracy.com>
2021-04-22 20:53:54 -0400

    Add two initial pytest fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from nebracy import create_app
+
+
+@pytest.fixture(scope='module')
+def app():
+    app = create_app('development', 'static')
+    with app.app_context():
+        yield app
+
+
+@pytest.fixture(scope='module')
+def client(app):
+    yield app.test_client()
