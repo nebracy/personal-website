30e57b452269bbc736f15ffdf5be9f81b761d936
Nicole B <contact@nebracy.com>
2021-04-23 11:44:23 -0400

    Remove and refactor some asserts from first test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,11 @@
+import pytest
 from flask import url_for
 
 
 def test_home_get(client):
     """Given When Then"""
-    assert client.get('/').status_code == 200
-    response = client.get(url_for('home.index', _external=True, _scheme='https'))
-    assert response.status_code == 200
-    assert b"Welcome!" in response.data
-    assert url_for('home.index', _external=True, _scheme='https') == 'https://localhost/'
+    home_url = url_for('home.index', _external=True, _scheme='https')
+    assert home_url == 'https://localhost/'
+    assert client.get(home_url).status_code == 200
+    assert b"Welcome!" in client.get(home_url).data
     assert url_for('static', filename='favicon.ico', _external=True, _scheme='https') == 'https://localhost/favicon.ico'
-
-
-
