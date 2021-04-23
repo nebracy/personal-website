f4cf19fa5809a81bf74433431c405e5e04f9903b
Nicole B <contact@nebracy.com>
2021-04-23 11:52:40 -0400

    Add basic get test for webhook route & add sample payload for later webhook tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,15 @@ import pytest
 from flask import url_for
 
 
+payload = {"ref": "refs/heads/master", "repository": {
+        "name": "personal-website",
+        "url": "https://github.com/nebracy/personal-website"},
+        "commits": [{
+            "id": "b550065594f767a9275b4c9a18810fd2d1479b74",
+            "message": "Rename config class names",
+            "timestamp": "2019-12-29T21:36:16-05:00"}]}
+
+
 def test_home_get(client):
     """Given When Then"""
     home_url = url_for('home.index', _external=True, _scheme='https')
@@ -9,3 +18,9 @@ def test_home_get(client):
     assert client.get(home_url).status_code == 200
     assert b"Welcome!" in client.get(home_url).data
     assert url_for('static', filename='favicon.ico', _external=True, _scheme='https') == 'https://localhost/favicon.ico'
+
+
+def test_webhook_get(client):
+    response = client.get('/webhook')
+    assert response.status_code == 404
+
