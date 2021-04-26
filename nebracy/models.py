c89e76d78dafe59a36279624da2d67428ad892be
Nicole B <contact@nebracy.com>
2021-04-26 14:04:21 -0400

    Split new GithubCommits class from Commit with most of its methods

--- nebracy/models.py
+++ nebracy/models.py
@@ -15,7 +15,6 @@ class Commit(db.Model):
     url = db.Column(db.String(100), nullable=False)
     date = db.Column(db.DateTime, nullable=False)
     msg = db.Column(db.String(75), nullable=False)
-    github = Github(os.getenv('GITHUB_TOKEN'))
 
     def __init__(self, commit_id=None, name=None, url=None, date=None, msg=None):
         self.commit_id = commit_id
@@ -23,29 +22,35 @@ class Commit(db.Model):
         self.url = url
         self.date = date
         self.msg = msg
-        self.commit_list = []
 
     def __repr__(self):
         return f'<Commit {self.commit_id}, {self.name}, {self.url}, {self.date}, {self.msg}>'
 
-    def __len__(self):
-        return len(self.commit_list)
 
-    def get_commits_per_repo(self, num, months_ago=6):
+class GithubCommits:
+    __tablename__ = 'github commits'
+    github = Github(os.getenv('GITHUB_TOKEN'))
+
+    def __init__(self, commit_num=3):
+        self.commit_num = commit_num
+        self.list = []
+        self.commit = Commit
+
+    def __repr__(self):
+        return f'<GithubCommits {self.commit_num}>'
+
+    def __len__(self):
+        return len(self.list)
+
+    def get_commits_per_repo(self, months_ago=6):
         num_months_ago = datetime.today() - relativedelta(months=months_ago)
-        for repo in Commit.github.get_user().get_repos():
-            commits = repo.get_commits()[:num]
+        for repo in GithubCommits.github.get_user().get_repos():
+            commits = repo.get_commits()[:self.commit_num]
             for c in commits:
                 if c.commit.committer.date > num_months_ago:
                     commit_date = self.convert_tz(c.commit.committer.date)
-                    self.commit_list.append({'id': c.commit.sha, 'name': repo.full_name, 'url': repo.html_url,
-                                            'date': commit_date, 'msg': c.commit.message})
-
-    def add_initial_commits(self):
-        for commit in self.commit_list:
-            c = Commit(commit['id'], commit['name'], commit['url'], commit['date'], commit['msg'])
-            db.session.add(c)
-        db.session.commit()
+                    self.list.append({'id': c.commit.sha, 'name': repo.full_name, 'url': repo.html_url,
+                                      'date': commit_date, 'msg': c.commit.message})
 
     @staticmethod
     def convert_tz(unconverted_date):
@@ -53,18 +58,23 @@ class Commit(db.Model):
         est_date = utc_date.astimezone(pytz.timezone('America/New_York'))
         return est_date
 
-    def sort_list(self, num):
-        final_list = sorted(self.commit_list, key=lambda commit: commit['date'], reverse=True)[:3]
-        self.commit_list = final_list[:num]
+    def sort_list(self):
+        final_list = sorted(self.list, key=lambda commit: commit['date'], reverse=True)[:3]
+        self.list = final_list[:self.commit_num]
+
+    def add_to_db(self):
+        for commit in self.list:
+            c = self.commit(commit['id'], commit['name'], commit['url'], commit['date'], commit['msg'])
+            db.session.add(c)
+        db.session.commit()
 
 
 @event.listens_for(Commit.__table__, 'after_create')
 def autofill_table(*args, **kwargs):
-    commit = Commit()
-    max_num = 3
-    commit.get_commits_per_repo(max_num)
-    commit.sort_list(max_num)
-    commit.add_initial_commits()
+    github_commits = GithubCommits()
+    github_commits.get_commits_per_repo()
+    github_commits.sort_list()
+    github_commits.add_to_db()
 
 
 
