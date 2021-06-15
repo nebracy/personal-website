000f535dc39a6d7a50d48de4f9a7ed246892c96f
Nicole B <contact@nebracy.com>
2021-06-14 23:20:35 -0400

    Add empty docstrings for later and remove unused imports

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,17 +1,17 @@
 from datetime import datetime
-import os
-import pytest
 import pytz
 from nebracy.models import Commit, GithubCommits
 
 
 def test_commit_class(client):
+    """"""
     date = datetime.strptime('2021-04-20 13:27:16', '%Y-%m-%d %H:%M:%S')
     commit = Commit('778df45a6d', 'nebracy/personal-website', 'https://github.com/nebracy/personal-website', date, 'Test commit message')
     assert commit.name == 'nebracy/personal-website'
 
 
-# def test_query_order_limit(client):       TODO fails for now, update
+# def test_query_order_limit(client):       TODO update test, currently fails
+#     """"""
 #     commit = Commit.query.order_by(Commit.date.desc()).limit(3).all()
 #     assert len(commit) == 3
 #     # check commits from most recent
