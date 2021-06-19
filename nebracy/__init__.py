ee0df98b9fd7cc7cc18c1c794e81d9ca80c44bb5
Nicole B <contact@nebracy.com>
2021-06-19 11:09:04 -0400

    Change if statement to be more clear, skip if using Development configuration

--- nebracy/__init__.py
+++ nebracy/__init__.py
@@ -11,7 +11,7 @@ def create_app(config='Production'):
     static_folder = 'static' if config.capitalize() == 'Development' else None
     app = Flask(__name__, static_url_path='', static_folder=static_folder)
     app.config.from_object(f'config.{config.capitalize()}')
-    if not static_folder:
+    if config.capitalize() != 'Development':
         app.static_folder = app.config.get('S3_FOLDER')
         app.add_url_rule(f'{app.config.get("STATIC_PATH")}/<path:filename>',
                          endpoint='static',
