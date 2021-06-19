3b1bbc6a222809ffb27fbf47977bc17312ba6aef
Nicole B <contact@nebracy.com>
2021-06-18 20:32:54 -0400

    Add and update for static_path argument in create_app func

--- tests/test_config.py
+++ tests/test_config.py
@@ -10,13 +10,13 @@ def test_config_development(app):
 
 
 def test_config_staging():
-    app = create_app('staging')
+    app = create_app('staging', '/staging')
     app.config["SERVER_NAME"] = 'test.nebracy.com'
     with app.app_context():
         assert app.config["S3_FOLDER"] == 'static/staging'
         assert app.config["ENV"] == 'production'
         assert url_for('home.index', _external=True, _scheme='https') == 'https://test.nebracy.com/'
-        assert url_for('static', filename='favicon.ico', _external=True, _scheme='https') == 'https://static.test.nebracy.com/favicon.ico'
+        assert url_for('static', filename='favicon.ico', _external=True, _scheme='https') == 'https://static.test.nebracy.com/staging/favicon.ico'
 
 
 def test_config_production():
