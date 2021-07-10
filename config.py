3ca5c00997237210d32a133a6cf59881555a94dc
Nicole B <contact@nebracy.com>
2021-07-10 08:46:13 -0400

    Add MAIL_RECIPIENT variable & change default MAIL_SERVER to gmail

--- config.py
+++ config.py
@@ -8,10 +8,11 @@ class Config:
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_SERVER = os.getenv('FLASK_MAIL_SERVER', 'smtppro.zoho.com')
+    MAIL_SERVER = os.getenv('FLASK_MAIL_SERVER', 'smtp.gmail.com')
     MAIL_USERNAME = os.getenv('FLASK_MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('FLASK_MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.getenv('FLASK_MAIL_DEFAULT_SENDER')
+    MAIL_DEFAULT_SENDER = os.getenv('FLASK_MAIL_DEFAULT_SENDER', 'contact@nebracy.com')
+    MAIL_RECIPIENT = os.getenv('FLASK_MAIL_RECIPIENT')
     SQLALCHEMY_DATABASE_URI = os.getenv('FLASK_DB_URI', f'sqlite:///{os.path.join(basedir, "github.db")}')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STATIC_PATH = ''
@@ -29,7 +30,6 @@ class Staging(Config):
 class Development(Config):
     S3_FOLDER = None
     SECRET_KEY = 'default-secret-key'
-    MAIL_SERVER = 'smtp.gmail.com'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     TESTING = True
     PRESERVE_CONTEXT_ON_EXCEPTION = False
--- nebracy/views.py
+++ nebracy/views.py
@@ -27,7 +27,7 @@ def index():
     if contact_form.validate_on_submit():
         msg = Message(contact_form.subj.data,
                       sender=(contact_form.name.data, app.config['MAIL_DEFAULT_SENDER']),
-                      recipients=[app.config['MAIL_DEFAULT_SENDER']],
+                      recipients=[app.config['MAIL_RECIPIENT']],
                       reply_to=contact_form.email.data)
         msg.body = contact_form.msg.data
         mail.send(msg)
