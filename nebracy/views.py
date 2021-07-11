793fa03a4c9f28e37c925e8507ceef1f46aa2960
Nicole B <contact@nebracy.com>
2021-07-11 16:27:22 -0400

    Add to subject of all sent emails Contact Form:

--- nebracy/views.py
+++ nebracy/views.py
@@ -25,7 +25,7 @@ def index():
         db_commits = []
     contact_form = ContactForm()
     if contact_form.validate_on_submit():
-        msg = Message(contact_form.subj.data,
+        msg = Message(f'Contact Form: {contact_form.subj.data}',
                       sender=(contact_form.name.data, app.config['MAIL_DEFAULT_SENDER']),
                       recipients=[app.config['MAIL_RECIPIENT']],
                       reply_to=contact_form.email.data)
