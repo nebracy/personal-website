ac9c81871da8504d090bf9456df41b226344e592
Nicole B <contact@nebracy.com>
2021-07-12 15:00:57 -0400

    Change const from min to max width and refactor

--- nebracy/static/js/script.js
+++ nebracy/static/js/script.js
@@ -1,15 +1,14 @@
 const navDropdown = document.getElementById("dropdown");
-const minWidth = window.matchMedia("(min-width: 650px)");
+const maxWidth = window.matchMedia("(max-width: 650px)");
 
 function toggleDropdown(event) {
-    var maxWidth = window.matchMedia("(max-width: 650px)");
     if (event.target.closest("#dropbtn") && maxWidth.matches) {    // Screen less than 650px
         navDropdown.classList.toggle("nav__menu--show");
       }
 }
 
 function checkScreenSize(event) {
-    if (event.matches) {     // Screen greater than 650px
+    if (!event.matches) {     // Screen (greater/not less) than 650px
         if (navDropdown.classList.contains("nav__menu--show")) {
             navDropdown.classList.remove("nav__menu--show");
         }
@@ -17,4 +16,4 @@ function checkScreenSize(event) {
 }
 
 document.addEventListener("click", toggleDropdown)
-minWidth.addEventListener("change", checkScreenSize)
+maxWidth.addEventListener("change", checkScreenSize)
