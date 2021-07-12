000922230590c2d7d4cfa293217de38f90b4b197
Nicole B <contact@nebracy.com>
2021-07-12 14:20:19 -0400

    Replace deprecated addListener in checkScreenSize func & remove else block in other func

--- nebracy/static/js/script.js
+++ nebracy/static/js/script.js
@@ -5,16 +5,12 @@ function toggleDropdown(event) {
     var width = window.matchMedia("(max-width: 650px)");
     if (event.target.closest("#dropbtn") && width.matches) {    // Screen less than 650px
         element.classList.toggle("nav__menu--show");
-    } else {
-        if (element.classList.contains("nav__menu--show")) {
-            element.classList.remove("nav__menu--show");
-        }
-    }
+      }
 }
 
 function checkScreenSize() {
     var width = window.matchMedia("(min-width: 650px)");
-    width.addListener(checkScreenSize);
+    width.addEventListener("change", checkScreenSize);
     if (width.matches) {     // Greater than 650px
         var element = document.getElementById("dropdown");
         if (element.classList.contains("nav__menu--show")) {
