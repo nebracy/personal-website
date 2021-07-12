dab9e778e7c58174451cbe86c7682f2122c547f0
Nicole B <contact@nebracy.com>
2021-07-12 14:45:06 -0400

    Refactor and replace var with const

--- nebracy/static/js/script.js
+++ nebracy/static/js/script.js
@@ -1,9 +1,7 @@
-document.addEventListener("click", toggleDropdown)
-window.matchMedia("(min-width: 650px)").addEventListener("change", checkScreenSize)
-
+const element = document.getElementById("dropdown");
+const minWidth = window.matchMedia("(min-width: 650px)");
 
 function toggleDropdown(event) {
-    var element = document.getElementById("dropdown");
     var width = window.matchMedia("(max-width: 650px)");
     if (event.target.closest("#dropbtn") && width.matches) {    // Screen less than 650px
         element.classList.toggle("nav__menu--show");
@@ -12,9 +10,11 @@ function toggleDropdown(event) {
 
 function checkScreenSize(event) {
     if (event.matches) {     // Screen greater than 650px
-        var element = document.getElementById("dropdown");
         if (element.classList.contains("nav__menu--show")) {
             element.classList.remove("nav__menu--show");
         }
     }
 }
+
+document.addEventListener("click", toggleDropdown)
+minWidth.addEventListener("change", checkScreenSize)
