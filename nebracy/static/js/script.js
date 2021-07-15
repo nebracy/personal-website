d30cdd12034e6866136ae60e5379c0b5f9011b6a
Nicole B <contact@nebracy.com>
2021-07-15 00:26:02 -0400

    Add else if to close menu dropdown if clicking anywhere else

--- nebracy/static/js/script.js
+++ nebracy/static/js/script.js
@@ -6,6 +6,9 @@ function toggleDropdown(event) {
     if (event.target.closest("#dropbtn") && maxWidth.matches) {    // Screen less than 650px
         navDropdown.classList.toggle("nav__menu--show");
         navHamburger.classList.toggle("nav__hamburger--active");
+      } else if (maxWidth.matches && navDropdown.classList.contains("nav__menu--show")) {
+            navDropdown.classList.remove("nav__menu--show");
+            navHamburger.classList.remove("nav__hamburger--active");
       }
 }
 
