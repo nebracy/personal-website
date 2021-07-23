c5fe3087fd643f13480dbbd750d13f0c655a955c
Nicole B <contact@nicolebracy.com>
2021-07-22 23:28:42 -0400

    Dim screen on opening dropdown menu on small mobile devices

--- nebracy/static/js/script.js
+++ nebracy/static/js/script.js
@@ -1,14 +1,17 @@
 const navDropdown = document.getElementById("dropdown");
 const navHamburger = document.getElementById("hamburger");
+const screenOverlay = document.getElementById("overlay");
 const maxWidth = window.matchMedia("(max-width: 650px)");
 
 function toggleDropdown(event) {
     if (event.target.closest("#dropbtn") && maxWidth.matches) {    // Screen less than 650px
         navDropdown.classList.toggle("nav__menu--show");
         navHamburger.classList.toggle("nav__hamburger--active");
+        screenOverlay.classList.toggle("screen-overlay--dim");
       } else if (maxWidth.matches && navDropdown.classList.contains("nav__menu--show")) {
             navDropdown.classList.remove("nav__menu--show");
             navHamburger.classList.remove("nav__hamburger--active");
+            screenOverlay.classList.remove("screen-overlay--dim");
       }
 }
 
@@ -16,6 +19,7 @@ function checkScreenSize(event) {
     if (!event.matches && navDropdown.classList.contains("nav__menu--show")) {     // Screen (greater/not less) than 650px
         navDropdown.classList.remove("nav__menu--show");
         navHamburger.classList.remove("nav__hamburger--active");
+        screenOverlay.classList.remove("screen-overlay--dim");
     }
 }
 
