806f9637363662c1e5cb03082c53c6fe3dc11b06
Nicole B <contact@nicolebracy.com>
2021-07-24 03:08:08 -0400

    Refactor both functions

--- nebracy/static/js/script.js
+++ nebracy/static/js/script.js
@@ -3,24 +3,22 @@ const navHamburger = document.getElementById("hamburger");
 const screenOverlay = document.getElementById("overlay");
 const maxWidth = window.matchMedia("(max-width: 650px)");
 
+function checkScreenSize() {
+    if (navDropdown.classList.contains("nav__menu--show")) {
+        navDropdown.classList.remove("nav__menu--show");
+        navHamburger.classList.remove("nav__hamburger--x");
+        screenOverlay.classList.remove("screen-overlay--dim");
+    }
+}
+
 function toggleDropdown(event) {
     if (event.target.closest("#dropbtn") && maxWidth.matches) {    // Screen less than 650px
         navDropdown.classList.toggle("nav__menu--show");
         navHamburger.classList.toggle("nav__hamburger--x");
         screenOverlay.classList.toggle("screen-overlay--dim");
-      } else if (maxWidth.matches && navDropdown.classList.contains("nav__menu--show")) {
-            navDropdown.classList.remove("nav__menu--show");
-            navHamburger.classList.remove("nav__hamburger--x");
-            screenOverlay.classList.remove("screen-overlay--dim");
+      } else if (maxWidth.matches) {
+            checkScreenSize()
       }
-}
-
-function checkScreenSize(event) {
-    if (!event.matches && navDropdown.classList.contains("nav__menu--show")) {     // Screen (greater/not less) than 650px
-        navDropdown.classList.remove("nav__menu--show");
-        navHamburger.classList.remove("nav__hamburger--x");
-        screenOverlay.classList.remove("screen-overlay--dim");
-    }
 }
 
 document.addEventListener("click", toggleDropdown)
