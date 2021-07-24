41e5e8f44e954d17c2e4c37b019ef542d3b5c62d
Nicole B <contact@nicolebracy.com>
2021-07-24 01:57:07 -0400

    Rename hamburger modifier

--- nebracy/static/js/script.js
+++ nebracy/static/js/script.js
@@ -6,11 +6,11 @@ const maxWidth = window.matchMedia("(max-width: 650px)");
 function toggleDropdown(event) {
     if (event.target.closest("#dropbtn") && maxWidth.matches) {    // Screen less than 650px
         navDropdown.classList.toggle("nav__menu--show");
-        navHamburger.classList.toggle("nav__hamburger--active");
+        navHamburger.classList.toggle("nav__hamburger--x");
         screenOverlay.classList.toggle("screen-overlay--dim");
       } else if (maxWidth.matches && navDropdown.classList.contains("nav__menu--show")) {
             navDropdown.classList.remove("nav__menu--show");
-            navHamburger.classList.remove("nav__hamburger--active");
+            navHamburger.classList.remove("nav__hamburger--x");
             screenOverlay.classList.remove("screen-overlay--dim");
       }
 }
@@ -18,7 +18,7 @@ function toggleDropdown(event) {
 function checkScreenSize(event) {
     if (!event.matches && navDropdown.classList.contains("nav__menu--show")) {     // Screen (greater/not less) than 650px
         navDropdown.classList.remove("nav__menu--show");
-        navHamburger.classList.remove("nav__hamburger--active");
+        navHamburger.classList.remove("nav__hamburger--x");
         screenOverlay.classList.remove("screen-overlay--dim");
     }
 }
