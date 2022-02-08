e647185ab9546fe49d1e292a9944c4280fdb66ef
Nicole B <contact@nicolebracy.com>
2022-02-08 17:13:45 -0500

    Add fields for pizza dough calculator & more imports

--- nebracy/apps/forms.py
+++ nebracy/apps/forms.py
@@ -1,7 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import DecimalField, DecimalRangeField, FieldList, IntegerRangeField, RadioField, StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import DecimalField, DecimalRangeField, FieldList, IntegerField, IntegerRangeField, RadioField, StringField, SubmitField
+from wtforms.validators import InputRequired, NumberRange, Optional
 
 
 class DoughCalculatorForm(FlaskForm):
-    pass
+    choice = RadioField('TF/Weight', choices=['Dough Weight', 'Thickness Factor'], validators=[InputRequired()])
+    dough_weight = DecimalField('Dough Weight', validators=[Optional(), NumberRange(0)])
+    thickness_factor = DecimalRangeField('Thickness Factor', validators=[Optional(), NumberRange(0.07, 0.1)])
+    pizza_size = IntegerField('Pizza Size (in)', validators=[Optional(), NumberRange(12, 22)])
+    g_oz = RadioField('Grams/Ounces', choices=['grams', 'ounces'], validators=[InputRequired()])
+    pizza_num = IntegerField('Pizza(s)', validators=[InputRequired(), NumberRange(1, 25)])
+    water = IntegerRangeField('Water %', description='Ingredient', validators=[InputRequired(), NumberRange(55, 70)])
+    yeast = DecimalField('Yeast %', description='Ingredient', validators=[InputRequired(), NumberRange(0, 3)])
+    salt = DecimalField('Salt %', description='Ingredient', validators=[InputRequired(), NumberRange(0, 4)])
+    oil = DecimalField('Oil %', description='Ingredient', validators=[Optional(), NumberRange(0, 8)])
+    sugar = DecimalField('Sugar %', description='Ingredient', validators=[Optional(), NumberRange(0, 4)])
+    submit = SubmitField('Send')
