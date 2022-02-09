24d63845ea1b2cab42bb793a15790274fe5d5ea4
Nicole B <contact@nicolebracy.com>
2022-02-09 02:15:51 -0500

    Set Thickness Factor as the default radio choice

--- nebracy/apps/forms.py
+++ nebracy/apps/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import InputRequired, NumberRange, Optional
 
 
 class DoughCalculatorForm(FlaskForm):
-    choice = RadioField('TF/Weight', choices=['Dough Weight', 'Thickness Factor'], validators=[InputRequired()])
+    choice = RadioField('TF/Weight', choices=['Dough Weight', 'Thickness Factor'], default='Thickness Factor', validators=[InputRequired()])
     dough_weight = DecimalField('Dough Weight', validators=[Optional(), NumberRange(0.01)])
     g_oz = RadioField('Grams/Ounces', choices=['grams', 'ounces'], validators=[Optional()])
     thickness_factor = DecimalRangeField('Thickness Factor', validators=[Optional(), NumberRange(0.07, 0.1)])
