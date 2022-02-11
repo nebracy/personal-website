e1c71aff5af580f68f8c5e8ef87ad19c2bfe1a27
Nicole B <contact@nicolebracy.com>
2022-02-11 06:48:14 -0500

    Replace previous validator with custom class validator

--- nebracy/apps/forms.py
+++ nebracy/apps/forms.py
@@ -3,12 +3,26 @@ from wtforms import DecimalField, DecimalRangeField, FieldList, IntegerField, In
 from wtforms.validators import InputRequired, NumberRange, Optional
 
 
+class RequiredIf:
+    def __init__(self, message=None):
+        self.message = message
+
+    def __call__(self, form, field):
+        choice = form['choice'].data
+        if field.id in ['thickness_factor', 'pizza_size'] and choice == 'Thickness Factor':
+            InputRequired().__call__(form, field)
+        elif field.id in ['dough_weight', 'g_oz'] and choice == 'Dough Weight':
+            InputRequired().__call__(form, field)
+        else:
+            Optional().__call__(form, field)
+
+
 class DoughCalculatorForm(FlaskForm):
     choice = RadioField('TF/Weight', choices=['Dough Weight', 'Thickness Factor'], default='Thickness Factor', validators=[InputRequired()])
-    dough_weight = DecimalField('Dough Weight', validators=[Optional(), NumberRange(1, 20000)])
-    g_oz = RadioField('Grams/Ounces', choices=['oz', 'grams'], default='grams', validators=[Optional()])
-    thickness_factor = DecimalRangeField('Thickness Factor', validators=[Optional(), NumberRange(0.07, 0.1)])
-    pizza_size = IntegerField('Pizza Size (in)', validators=[Optional(), NumberRange(12, 22)])
+    dough_weight = DecimalField('Dough Weight', validators=[RequiredIf(), NumberRange(1, 20000)])
+    g_oz = RadioField('Grams/Ounces', choices=['oz', 'grams'], default='grams', validators=[RequiredIf()])
+    thickness_factor = DecimalRangeField('Thickness Factor', validators=[RequiredIf(), NumberRange(0.07, 0.1)])
+    pizza_size = IntegerField('Pizza Size (in)', validators=[RequiredIf(), NumberRange(12, 22)])
     pizza_num = IntegerField('Pizza(s)', validators=[InputRequired(), NumberRange(1, 25)])
     water = IntegerRangeField('Water %', description='Ingredient', validators=[InputRequired(), NumberRange(55, 70)])
     yeast = DecimalField('Yeast %', description='Ingredient', validators=[InputRequired(), NumberRange(0, 3)])
