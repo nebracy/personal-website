41b9db81ab911eb182d5963fd2e4e9594ad80501
Nicole B <contact@nicolebracy.com>
2022-02-11 23:54:05 -0500

    Delete funcs and listeners for app

--- nebracy/static/js/script.js
+++ nebracy/static/js/script.js
@@ -3,8 +3,6 @@ const navHamburger = document.getElementById("hamburger");
 const screenOverlay = document.getElementById("overlay");
 const maxWidth = window.matchMedia("(max-width: 650px)");
 
-const slider = document.getElementById('thk_factor');
-
 
 function checkScreenSize() {
     if (navDropdown.classList.contains("nav__menu--show")) {
@@ -23,63 +21,5 @@ document.addEventListener("click", e => {
             checkScreenSize();
       }
 });
-
-
-function updateSliderValue() {
-    var sliderValue  = slider.value;
-    document.getElementsByName("tf")[0].textContent = (sliderValue);
-}
-
-
-function tfWeight() {
-    doughWt = document.getElementById('dough_wt');
-    pizzaSz = document.getElementById('pizza_size');
-
-    if (document.getElementById('choice-0').checked) {
-        document.getElementById('choice-wt').style.display = "block";
-        document.getElementById('choice-tf').style.display = "none";
-        pizzaSz.required = false;
-        doughWt.required = true;
-        pizzaSz.value = 16;
-
-    }
-    else {
-        document.getElementById('choice-wt').style.display = "none";
-        document.getElementById('choice-tf').style.display = "block";
-        pizzaSz.required = true;
-        doughWt.required = false;
-        doughWt.value = "";
-    }
-}
-
-tfWeight();
-
-
-function printTable() {
-     var divToPrint=document.getElementById("recipe");
-        newWin= window.open("", "", 'height=500, width=750');
-        newWin.document.write(divToPrint.outerHTML);
-        newWin.print();
-        newWin.close();
-}
-
-
-function hideShowPrint() {
-    if (document.getElementById('recipe')) {
-        document.getElementById('print').hidden = false;
-    }
-    else {
-        document.getElementById('print').hidden = true;
-    }
-}
-
-hideShowPrint();
-
-
-document.getElementById("print").addEventListener("click", printTable);
-
-document.getElementById('choice').addEventListener("change", tfWeight);
-
-slider.addEventListener("input", updateSliderValue);
 
 maxWidth.addEventListener("change", checkScreenSize);
